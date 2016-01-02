c24100114f87f2a70659faaacd244c836449bf6f
ayerman <ayerman@kent.edu>
2016-01-02 18:57:27 -0500

    more work

---
+++ CollageWebsite/CollageWebsite/Scripts/site.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $(".toggle_container").hide();
+
+    $("p.trigger").click(function () {
+        $(this).toggleClass("active").next().slideToggle("normal");
+    });
+});
